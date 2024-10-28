6fd9a3807f6d41caaca8fe6694b5d95592ef487f
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-10-28 18:42:16 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticProps } from 'next';
-
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +28,72 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <main className={commonStyles.container}>
+      <div className={styles.posts}>
+        {postsPagination.results.map(post => (
+          <Link key={post.uid} href={`/post/${post.uid}`}>
+            <a>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div className={styles.info}>
+                <time>
+                  <FiCalendar size={20} /> {post.first_publication_date}
+                </time>
+                <span>
+                  <FiUser size={20} /> {post.data.author}
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      {postsPagination.next_page && (
+        <button type="button" className={styles['load-posts-button']}>
+          Carregar mais posts
+        </button>
+      )}
+    </main>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps<{
+  postsPagination: PostPagination;
+}> = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 4,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => ({
+    uid: post.uid,
+    first_publication_date: format(
+      new Date(post.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+  }));
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results: posts,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
